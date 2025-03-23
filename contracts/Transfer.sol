141d28cb1c3ba7c9139e20b29ec32e98e0614123
Sarthak468 <sarthakbarot404@gmail.com>
2025-03-23 17:44:09 -0600

    Initial commit

---
+++ contracts/Transfer.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.19;
+
+contract Transfer {
+    function transfer(address payable to) external payable {
+        to.transfer(msg.value);
+    }
+}
